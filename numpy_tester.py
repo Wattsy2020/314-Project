c664ccdc0e1f14dee0b1365cd5036b80d9f2da6c
Liam Watts <liam.watts271@gmail.com>
2020-06-10 12:22:55 +1000

    Refactor run_tests to work with any module

--- numpy_tester.py
+++ numpy_tester.py
@@ -2,14 +2,15 @@ import inspect
 import linear_algebra_tests
 
 # note in future needs to record and print number of failures and where
-def run_tests():
-    # get list of all test functions and their names in linear_algebra test
-    members = inspect.getmembers(linear_algebra_tests)
+def run_tests(module):
+    # get list of all test functions and their names in a module
+    members = inspect.getmembers(module)
     test_functions = list(filter(lambda x: x[0][:5] == "test_", members))
+    module_name = list(filter(lambda x: x[0][:8] == "__name__", members))[0][1]
     
     # loop through and run each test 100 times
-    print("Testing linear_algebra_tests.py\n")
-    print("{0: <30}Status".format("Function"))
+    print("Testing {}.py\n".format(module_name))
+    print("{0: <30}Status".format("Test Name"))
     for name, function in test_functions:
         print("{0: <30}".format(name), end="")
         try:
@@ -21,4 +22,4 @@ def run_tests():
             print("Pass")
     
 if __name__ == "__main__":
-    run_tests()
+    run_tests(linear_algebra_tests)
